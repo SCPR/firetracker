ad4a61e27f8faa75d3d0e9b40c75cb18a12d7abe
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-02 11:39:36 -0700

    updates api controllers

--- firetracker/api.py
+++ firetracker/api.py
@@ -2,12 +2,12 @@ from tastypie.resources import ModelResource
 from tastypie import fields
 from tastypie.constants import ALL, ALL_WITH_RELATIONS
 from tastypie.serializers import Serializer
-from wildfires.models import Wildfire
+from calfire_tracker.models import CalWildfire
 
-class WildfireResource(ModelResource):
+class CalWildfireResource(ModelResource):
     class Meta:
-        queryset = Wildfire.objects.all()
-        resource_name = 'wildfire'
+        queryset = CalWildfire.objects.all()
+        resource_name = 'wildfires'
         #fields = ['fire_name', 'county', 'acres_burned', 'containment_percent', 'date_started', 'last_update']
         allowed_methods = ['get']
         limit = 100
