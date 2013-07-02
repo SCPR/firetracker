2c55f285f859b90b5c360a5618ea2d79d2357b52
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-02 09:27:37 -0700

    updates fabfile

--- fabfile.py
+++ fabfile.py
@@ -8,22 +8,11 @@ from fabric.colors import green
 
 def scrape():
     local('python manage.py scraper_wildfires')
-    local('heroku run python manage.py scraper_wildfires')
 
-def model_changed():
-    local('git cma "updates to models schema')
 
-    local('python manage.py convert_to_south wildfires')
-    local('python manage.py schemamigration wildfires --initial')
-    local('python manage.py migrate wildfires --fake')
 
-    local('git add .')
-    local('git cma "adds migration file')
-    local('git push')
 
-    local('heroku run python manage.py convert_to_south wildfires')
-    local('heroku run python manage.py schemamigration wildfires --initial')
-    local('heroku run python manage.py migrate wildfires --fake')
 
-def heroku_reset():
-    heroku pg:reset DATABASE
+
+
+
