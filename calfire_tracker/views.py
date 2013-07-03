8d125e4f11eabd429a39ef6a5c2821984f27d3d1
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-03 13:06:18 -0700

    adjusts view to provide all objects to template context on detail view

--- calfire_tracker/views.py
+++ calfire_tracker/views.py
@@ -1,4 +1,4 @@
-# Create your views here.
+from datetime import datetime, date, time, timedelta
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
@@ -6,21 +6,16 @@ from django.core import serializers
 from django.template import RequestContext
 from calfire_tracker.models import CalWildfire
 
-#the main index request
 def index(request):
-
-    #tells how the output should be structured
-    calwildfire_listing = CalWildfire.objects.all()
-
-    #takes template name as first argument
-    #returns an HttpResponse object of the given template
-    return render_to_response('index.html', {'calwildfire_listing': calwildfire_listing},
+    calwildfires = CalWildfire.objects.all()
+    return render_to_response('index.html', {'calwildfires': calwildfires},
         context_instance=RequestContext(request))
 
-#details request
 def detail(request, fire_slug):
-    calwildfire_listing = CalWildfire.objects.all()
-
     fire_detail = get_object_or_404(CalWildfire, fire_slug=fire_slug)
-    return render_to_response('detail.html', {'calwildfire': fire_detail},
+    startdate = date.today()
+    enddate = timedelta(days=21)
+    displaydate = startdate - enddate
+    calwildfires = CalWildfire.objects.filter(date_time_started__gte=displaydate)
+    return render_to_response('detail.html', {'calwildfire': fire_detail, 'calwildfires': calwildfires},
         context_instance=RequestContext(request))
