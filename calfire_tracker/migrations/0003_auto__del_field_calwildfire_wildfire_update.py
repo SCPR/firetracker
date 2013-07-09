7449df73aebdca1af5a35339292527d1d9f12210
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-09 10:14:10 -0700

    adds db migration files

---
+++ calfire_tracker/migrations/0003_auto__del_field_calwildfire_wildfire_update.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'CalWildfire.wildfire_update'
+        db.delete_column('calfire_tracker_calwildfire', 'wildfire_update_id')
+
+        # Adding M2M table for field wildfire_update on 'CalWildfire'
+        db.create_table('calfire_tracker_calwildfire_wildfire_update', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('calwildfire', models.ForeignKey(orm['calfire_tracker.calwildfire'], null=False)),
+            ('wildfireupdate', models.ForeignKey(orm['calfire_tracker.wildfireupdate'], null=False))
+        ))
+        db.create_unique('calfire_tracker_calwildfire_wildfire_update', ['calwildfire_id', 'wildfireupdate_id'])
+
+
+    def backwards(self, orm):
+        # Adding field 'CalWildfire.wildfire_update'
+        db.add_column('calfire_tracker_calwildfire', 'wildfire_update',
+                      self.gf('django.db.models.fields.related.ForeignKey')(to=orm['calfire_tracker.WildfireUpdate'], null=True, blank=True),
+                      keep_default=False)
+
+        # Removing M2M table for field wildfire_update on 'CalWildfire'
+        db.delete_table('calfire_tracker_calwildfire_wildfire_update')
+
+
+    models = {
+        'calfire_tracker.calwildfire': {
+            'Meta': {'object_name': 'CalWildfire'},
+            'acres_burned': ('django.db.models.fields.IntegerField', [], {'max_length': '8', 'null': 'True', 'blank': 'True'}),
+            'administrative_unit': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'cause': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'computed_location': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'conditions': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'containment_percent': ('django.db.models.fields.IntegerField', [], {'max_length': '4', 'null': 'True', 'blank': 'True'}),
+            'cooperating_agencies': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'county': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'created_fire_id': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            'current_situation': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'damage_assessment': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'date_time_started': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'evacuations': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'fire_name': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'fire_slug': ('django.db.models.fields.SlugField', [], {'max_length': '140', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'injuries': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'last_scraped': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'last_updated': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'location': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'location_geocode_error': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'location_latitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'location_longitude': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'more_info': ('django.db.models.fields.URLField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'notes': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'phone_numbers': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'promoted_fire': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'road_closures': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'structures_destroyed': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'structures_threatened': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'total_airtankers': ('django.db.models.fields.IntegerField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'total_dozers': ('django.db.models.fields.IntegerField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'total_fire_crews': ('django.db.models.fields.IntegerField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'total_fire_engines': ('django.db.models.fields.IntegerField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'total_fire_personnel': ('django.db.models.fields.IntegerField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'total_helicopters': ('django.db.models.fields.IntegerField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'total_water_tenders': ('django.db.models.fields.IntegerField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'training': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'twitter_hashtag': ('django.db.models.fields.CharField', [], {'max_length': '140', 'null': 'True', 'blank': 'True'}),
+            'wildfire_update': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['calfire_tracker.WildfireUpdate']", 'null': 'True', 'blank': 'True'})
+        },
+        'calfire_tracker.wildfireupdate': {
+            'Meta': {'object_name': 'WildfireUpdate'},
+            'date_time_update': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'fire_name': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'source': ('django.db.models.fields.URLField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'update_text': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['calfire_tracker']
