2e86595f2a3a11df624996b1148820919bddd031
Bryan Ricker <bricker88@gmail.com>
2013-07-12 12:12:04 -0700

    Set default DJANGO_SETTING_MODULE in passenger_wsgi

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,7 @@
 import os, sys, site
 
 REMOTE_APP_ROOT = "/web/archive/apps/firetracker"
+#REMOTE_APP_ROOT = "/Users/bryan/projects/firetracker"
 INTERP = "%s/virtualenv/firetracker/bin/python" % REMOTE_APP_ROOT
 
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
@@ -8,7 +9,7 @@ if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(REMOTE_APP_ROOT)
 sys.path.append('%s/firetracker' % REMOTE_APP_ROOT)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_production'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_production")
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
