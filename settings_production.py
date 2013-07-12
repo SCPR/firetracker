37c4958a1b9d992b2c38a49148a18b124b6334d2
Bryan Ricker <bricker88@gmail.com>
2013-07-12 13:33:07 -0700

    Add STATIC_ROOT to production settings

--- settings_production.py
+++ settings_production.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/web/archive/apps/firetracker/firetracker/public/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
