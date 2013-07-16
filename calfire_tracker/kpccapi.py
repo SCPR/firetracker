4f2af3e7a99835bc019a5553899b3a4fed755054
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-16 16:42:29 -0700

    adds class to create a kpcc article -- my first attempt at using a class -- and function to query kpcc article api and write to a list

--- calfire_tracker/kpccapi.py
+++ calfire_tracker/kpccapi.py
@@ -13,3 +13,33 @@ def search_assethost(kpcc_image_token, assethost_id):
     asset_photo_credit = js_object['owner']
     images_dict = {'asset_url_link': asset_url_link, 'asset_photo_credit': asset_photo_credit}
     return images_dict
+
+class kpcc_api_article():
+    def __init__(self, permalink, image_asset, publish_date, short_title):
+        self.user = permalink
+        self.text = image_asset
+        self.graphic = publish_date
+        self.time = short_title
+
+# search kpcc api for related articles to display on detail pages
+def search_kpcc_article_api():
+    url_prefix = 'http://www.scpr.org/api/v2/content/?'
+    url_types = 'types=news,blogs,segments'
+    url_query = '&query=wildfire+burning+california'
+    url_limit = '&limit=10'
+    url_page = '&page=1'
+    search_url = '%s%s%s%s%s' % (url_prefix, url_types, url_query, url_limit, url_page)
+    json_response = urllib.urlopen(search_url)
+    json_response = json_response.readlines()
+    js_object = json.loads(json_response[0])
+    articles = []
+    for kpcc_article in js_object:
+        article_permalink = kpcc_article['permalink']
+        article_image_asset = kpcc_article['assets'][0]['small']['url']
+        article_publish_date = kpcc_article['published_at']
+        article_short_title = kpcc_article['short_title']
+        this_article = kpcc_api_article(article_permalink, article_image_asset, article_publish_date, article_short_title)
+        articles.append(this_article)
+    return articles
+
+if __name__ == "__main__": search_kpcc_article_api()
