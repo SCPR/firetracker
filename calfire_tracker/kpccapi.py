5c7eebed87f62fc79fe810b1d46dfe0b2510e10a
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-15 19:02:20 -0700

    removes debugging from kpccapi script

--- calfire_tracker/kpccapi.py
+++ calfire_tracker/kpccapi.py
@@ -1,9 +1,5 @@
 import simplejson as json
 import urllib
-import logging
-
-# log everything and send to stderr
-logging.basicConfig(level=logging.DEBUG)
 
 # search assethost for an image
 def search_assethost(kpcc_image_token, assethost_id):
@@ -16,6 +12,4 @@ def search_assethost(kpcc_image_token, assethost_id):
     asset_url_link = js_object['urls']['full']
     asset_photo_credit = js_object['owner']
     images_dict = {'asset_url_link': asset_url_link, 'asset_photo_credit': asset_photo_credit}
-    logging.debug(images_dict)
     return images_dict
-if __name__ == "__main__": search_assethost()
