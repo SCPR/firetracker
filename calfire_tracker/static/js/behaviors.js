5d95ebf64c1cd4990d816e6f3a6dfe7280ce20e4
Jon White <mr.jon.white@gmail.com>
2013-07-16 11:14:48 -0700

    Added a way for JS to cover for overlong EVACUATIONS texts.

--- calfire_tracker/static/js/behaviors.js
+++ calfire_tracker/static/js/behaviors.js
@@ -5,31 +5,42 @@ jQuery(document).ready(function($) {
 	// Sometimes RESOURCES DEPLOYED will publish "n/a" values
 	// ============================================================
 	$(".deployed dl").each(function(){
-
 		var myResourceValue = $(this).find("dd");
 		if(myResourceValue.text() == "n/a") {
 			$(this).addClass("none");
 		}
-
 	});
+
 
 
 
 	// Sometimes we won't have any UPDATES TRACKER posts
 	// ============================================================
 	if ($(".tertiaries .updates").length) {
-		
 		if ($(".tertiaries .updates article").length) {
 			// do nothing; at least one has been published
 		} else {
 			$(".tertiaries .updates").remove();
 			$(".tertiaries").addClass("just-the-facts").removeClass("clearfix");
 		}
-
-		
 	}
 	
+	
+	
+	// Sometimes, "evacuations" text can just be too damn long.
+	// ============================================================
+	$(".fire-dashboard .damages .evacuations").each(function(){
 
+		var evacsMax = 250;
+		var myEvacsChars = $(this).find("dd").html().length;
+
+		if(myEvacsChars > evacsMax) {
+			$(".damages").addClass("spliced");
+			$("<aside class=\"fire-message\"></aside>").insertAfter(".fire-dashboard");
+			$(this).appendTo(".fire-message");
+		}
+
+	});
 
 
 
