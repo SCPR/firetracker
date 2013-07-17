87b1ada2b4a32630614f5a2490e2e550f3ccd351
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-17 07:52:50 -0700

    adds air quality as model class to database

--- calfire_tracker/models.py
+++ calfire_tracker/models.py
@@ -14,6 +14,9 @@ class CalWildfire(models.Model):
     promoted_fire = models.BooleanField('Feature This Fire?', default=False)
     asset_host_image_id = models.CharField('Image ID from Asset Host', max_length=140, null=True, blank=True)
     twitter_hashtag = models.CharField('Twitter Hashtag', max_length=140, null=True, blank=True)
+
+    air_quality_rating = models.IntegerField('air_quality_rating', max_length=3, null=True, blank=True)
+
     last_scraped = models.DateTimeField('Last Scraped', null=True, blank=True)
 
     # general details
