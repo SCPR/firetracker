4293c3ebf0edca00dfe224f664c8f26cfe9c5d08
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-18 17:20:53 -0700

    adds local_functions command to fab file to run functions in the functions_playground file

--- calfire_tracker/management/commands/functions_playground.py
+++ calfire_tracker/management/commands/functions_playground.py
@@ -35,19 +35,10 @@ data_dict_to_scan = [
     {'status_notes': '10,406 acres'},
 ]
 
-
-
-
-
 def loop_through_dict(list_of_dicts):
     for dict in list_of_dicts:
         for key, value in dict.iteritems():
             evaluate_for_percent_sign(key, value)
-
-
-
-
-
 
 def evaluate_for_percent_sign(dictionary_key, dictionary_value):
     ''' sees if a percent sign in present in a given string '''
@@ -69,25 +60,7 @@ def evaluate_for_percent_sign(dictionary_key, dictionary_value):
     logging.debug(message)
     return target_value
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+''' old functions I am building off of '''
 
 def evaluate_for_initial_digit(string_to_match):
     ''' runs regex on string to see it begins with a digit '''
@@ -121,27 +94,52 @@ def evaluate_for_acres_and_extract(string_to_match):
         target_number = 'exception'
     return target_number
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class Command(BaseCommand):
-    help = 'Scrapes California Wildfires data'
+    help = 'looping through dict for numbers'
     def handle(self, *args, **options):
         loop_through_dict(data_dict_to_scan)
+
+
+''' learning how the date come back with time zone info '''
+
+date_time_parse = 'July 18, 2013 9:45 am'
+
+def convert_time_to_nicey_format(date_time_parse):
+    ''' work crazy datetime magic that might be working '''
+    ''' based on http://stackoverflow.com/questions/17193228/python-twitter-api-tweet-timestamp-convert-from-utc-to-est '''
+    utc = timezone('UTC')
+    pacific = pytz.timezone('US/Pacific')
+
+    # check the date being pulled
+    logging.debug(date_time_parse)
+
+    # parse the date to datetime.datetime
+    date_time_parse = parser.parse(date_time_parse)
+    logging.debug(date_time_parse)
+
+    # register it as pacific time zone
+    pacificizd_date_time_parse = pacific.localize(date_time_parse)
+    logging.debug(pacificizd_date_time_parse)
+    logging.debug('%s - %s' % (pacificizd_date_time_parse.strftime("%A, %d, %B %Y %I:%M%p"), pacificizd_date_time_parse.tzinfo))
+
+    #logging.debug(date_time_parse.tzinfo)
+    #logging.debug('%s %s' % (utc_izd_datetime.strftime("%A, %d. %B %Y %I:%M%p"), utc_izd_datetime.tzinfo))
+    #logging.debug('%s %s' % (pacific_izd_datetime.strftime("%A, %d. %B %Y %I:%M%p"), pacific_izd_datetime.tzinfo))
+
+    #date_time_object = datetime.datetime(2013, 7, 18, 9, 45)
+    #logging.debug(date_time_object.strftime("%A, %d. %B %Y %I:%M%p"))
+
+    #target_datetime = utc.localize(parser.parse(date_to_parse))
+    #target_datetime = utc_datetime.astimezone(pacific)
+
+    #los_angeles = pytz.timezone('US/Pacific')
+    #target_datetime = los_angeles.localize(parser.parse(date_to_parse))
+
+    return pacificizd_date_time_parse
+
+'''
+class Command(BaseCommand):
+    help = 'tests dates'
+    def handle(self, *args, **options):
+        convert_time_to_nicey_format(date_time_parse)
+'''
