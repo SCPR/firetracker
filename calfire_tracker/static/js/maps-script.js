f7912c4aec48194db58389be690eb121134f3863
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-19 15:52:29 -0700

    pushes base map script to config

--- calfire_tracker/static/js/maps-script.js
+++ calfire_tracker/static/js/maps-script.js
@@ -1,18 +1,49 @@
-function initialize() {
-    var california = new google.maps.LatLng(34.01862413631862, -118.4208233779297);
-    var mapOptions = {
-        zoom: 11,
-        center: california,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+    function initialize() {
+
+
+        var initialZoom = 12;
+
+        // set zoom for mobile devices
+        if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
+            initialZoom = 7;
+        }
+
+        map = new google.maps.Map(document.getElementById('map_canvas'), {
+            center: new google.maps.LatLng(33.734048,-116.621643),
+            zoom: 12,
+            scrollwheel: false,
+            draggable: true,
+            mapTypeControl: false,
+            navigationControl: true,
+            streetViewControl: false,
+            panControl: false,
+            scaleControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            navigationControlOptions: {
+                style: google.maps.NavigationControlStyle.SMALL,
+                position: google.maps.ControlPosition.RIGHT_TOP}
+        });
+
+        var fireLayer = new google.maps.KmlLayer({
+            url: 'http://rmgsc.cr.usgs.gov/outgoing/geomac/ActiveFirePerimeters.kml',
+            preserveViewport: true
+        });
+
+        fireLayer.setMap(map);
+
+        google.maps.event.addDomListener(map, 'idle', function() {
+          calculateCenter();
+        });
+
+        google.maps.event.addDomListener(window, 'resize', function() {
+          map.setCenter(center);
+        });
+
     }
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    // function to maintain center point of map
+    function calculateCenter(){
+        center = map.getCenter();
+    };
 
-    var ctaLayer = new google.maps.KmlLayer({
-        url: 'http://rmgsc.cr.usgs.gov/outgoing/geomac/ActiveFirePerimeters.kml'
-    });
-
-    ctaLayer.setMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+    google.maps.event.addDomListener(window, 'load', initialize);
