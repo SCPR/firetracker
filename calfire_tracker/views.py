8b6cbbcdf1ecdb7116561e610e9b89cf21b4491b
Chris Keller <christopherlawrencekeller@gmail.com>
2013-07-19 12:24:38 -0700

    fixes all fires list on single fire view page for containment percent if inaccurate

--- calfire_tracker/views.py
+++ calfire_tracker/views.py
@@ -53,7 +53,7 @@ def index(request):
 
 def detail(request, fire_slug):
     calwildfire = get_object_or_404(CalWildfire, fire_slug=fire_slug)
-    calwildfires = CalWildfire.objects.all().order_by('containment_percent', '-date_time_started', 'fire_name')[:15]
+    calwildfires = CalWildfire.objects.exclude(containment_percent=None).order_by('containment_percent', '-date_time_started', 'fire_name')[0:15]
     wildfire_updates = WildfireUpdate.objects.filter(fire_name__fire_name=calwildfire.fire_name).order_by('-date_time_update')
     auth1 = tweepy.auth.OAuthHandler(settings.TWEEPY_CONSUMER_KEY, settings.TWEEPY_CONSUMER_SECRET)
     auth1.set_access_token(settings.TWEEPY_ACCESS_TOKEN, settings.TWEEPY_ACCESS_TOKEN_SECRET)
