a6299dbb10776a4367146a1947f68d45b0ca2267
chrislkeller <christopherlawrencekeller@gmail.com>
2013-08-08 20:27:24 -0700

    deactivates more_info in admin and activates notes in admin for mass edit

--- calfire_tracker/admin.py
+++ calfire_tracker/admin.py
@@ -16,7 +16,7 @@ class WildfireUpdateInline(admin.StackedInline):
     extra = 1
 
 class CalWildfireAdmin(admin.ModelAdmin):
-	list_display = ('fire_name', 'data_source', 'more_info', 'promoted_fire', 'asset_host_image_id', 'date_time_started', 'location_geocode_error', 'injuries', 'acres_burned', 'containment_percent', 'county', 'last_updated', 'last_scraped',)
+	list_display = ('fire_name', 'data_source', 'notes', 'promoted_fire', 'asset_host_image_id', 'date_time_started', 'location_geocode_error', 'injuries', 'acres_burned', 'containment_percent', 'county', 'last_updated', 'last_scraped',)
 	list_filter = ['data_source', 'county', 'date_time_started', 'last_updated']
 	search_fields = ['fire_name', 'county', 'acres_burned']
         inlines = (WildfireUpdateInline,)
