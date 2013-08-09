3b88d004df7c0ce8cf2f2f2eae03f86435af225e
chrislkeller <christopherlawrencekeller@gmail.com>
2013-08-08 20:52:15 -0700

    tests lockout mechanism on data model, to allow admin user to keep a fire from being overwritten automatically. Closes #117

--- calfire_tracker/admin.py
+++ calfire_tracker/admin.py
@@ -16,7 +16,8 @@ class WildfireUpdateInline(admin.StackedInline):
     extra = 1
 
 class CalWildfireAdmin(admin.ModelAdmin):
-	list_display = ('fire_name', 'data_source', 'promoted_fire', 'asset_host_image_id', 'date_time_started', 'location_geocode_error', 'injuries', 'acres_burned', 'containment_percent', 'county', 'last_updated', 'last_scraped', 'notes',)
+	list_display = ('fire_name', 'update_lockout', 'promoted_fire', 'asset_host_image_id', 'data_source', 'date_time_started', 'location_geocode_error', 'injuries', 'acres_burned', 'containment_percent',
+	    'county', 'last_updated', 'last_scraped', 'notes',)
 	list_filter = ['data_source', 'county', 'date_time_started', 'last_updated']
 	search_fields = ['fire_name', 'county', 'acres_burned']
         inlines = (WildfireUpdateInline,)
@@ -32,6 +33,7 @@ class CalWildfireAdmin(admin.ModelAdmin):
         fieldsets = [
             ('Management & Curation', {
                 'fields': [
+                    'update_lockout',
                     'promoted_fire',
                     'asset_host_image_id',
                     'twitter_hashtag',
