5c3f568867b70404697d9bef8916767384efecb1
Chris Keller <christopherlawrencekeller@gmail.com>
2013-08-27 13:27:24 -0700

    adds custom admin function for bulk updates of last save datetime

--- calfire_tracker/admin.py
+++ calfire_tracker/admin.py
@@ -1,5 +1,10 @@
 from calfire_tracker.models import CalWildfire, WildfireUpdate, WildfireTweet
 from django.contrib import admin
+from django.utils.timezone import utc, localtime
+import time, datetime, logging
+from datetime import tzinfo
+import pytz
+from pytz import timezone
 
 class WildfireTweetAdmin(admin.ModelAdmin):
 	list_display = ('tweet_screen_name', 'tweet_hashtag', 'tweet_created_at', 'tweet_text')
@@ -16,7 +21,7 @@ class WildfireUpdateInline(admin.StackedInline):
     extra = 1
 
 class CalWildfireAdmin(admin.ModelAdmin):
-	list_display = ('fire_name', 'update_lockout', 'promoted_fire', 'asset_host_image_id', 'data_source', 'date_time_started', 'location_geocode_error', 'injuries', 'acres_burned', 'containment_percent', 'county', 'last_updated', 'last_scraped', 'notes',)
+	list_display = ('fire_name', 'update_lockout', 'promoted_fire', 'asset_host_image_id', 'data_source', 'date_time_started', 'location_geocode_error', 'injuries', 'acres_burned', 'containment_percent', 'county', 'last_updated', 'last_scraped', 'notes', 'last_saved',)
 	list_filter = ['data_source', 'county', 'date_time_started', 'last_updated']
 	search_fields = ['fire_name', 'county', 'acres_burned']
         inlines = (WildfireUpdateInline,)
@@ -109,6 +114,7 @@ class CalWildfireAdmin(admin.ModelAdmin):
             'unfeature',
             'lock_fire_data',
             'unlock_fire_data',
+            'update_last_saved_time',
         ]
 
         def featured(self, request, queryset):
@@ -126,6 +132,11 @@ class CalWildfireAdmin(admin.ModelAdmin):
         def unlock_fire_data(self, request, queryset):
             queryset.update(update_lockout = False)
         unlock_fire_data.short_description = "Allow Auto Updates"
+
+        def update_last_saved_time(self, request, queryset):
+            date = datetime.datetime.now()
+            queryset.update(last_saved = date)
+        update_last_saved_time.short_description = "Update Last Saved Time"
 
 admin.site.register(WildfireTweet, WildfireTweetAdmin)
 admin.site.register(WildfireUpdate, WildfireUpdateAdmin)
