7230a10d828eb5fc95fa6060f12cfd97fc86955e
Chris Keller <christopherlawrencekeller@gmail.com>
2013-12-16 15:14:58 -0800

    updates fire data for 2013

--- calfire_tracker/views.py
+++ calfire_tracker/views.py
@@ -32,8 +32,8 @@ def index(request):
     featuredfires = wildfires.filter(promoted_fire=True).order_by('containment_percent', '-date_time_started', 'fire_name')[0:3]
     cache_timestamp = wildfires.all().order_by('-last_saved')
 
-    total_2013_fires = 6181
-    total_2013_acreage = 150782
+    total_2013_fires = 7009
+    total_2013_acreage = 120207
     total_2013_injuries = None
 
     total_2012_fires = 5809
