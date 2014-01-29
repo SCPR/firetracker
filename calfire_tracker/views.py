0d19193cb81bd2a0c23fd547396e0bb8e11a9383
Chris Keller <christopherlawrencekeller@gmail.com>
2014-01-28 21:03:09 -0800

    limits the list of fires on the error pages to the 20 most recent sorted by date. this closes #46

--- calfire_tracker/views.py
+++ calfire_tracker/views.py
@@ -156,19 +156,22 @@ def oembed(request):
     return HttpResponse(json.dumps(data), content_type="application/json")
 
 def custom_403(request):
-    calwildfires = CalWildfire.objects.all().order_by('-date_time_started', 'fire_name')
+    wildfires = CalWildfire.objects.all()
+    calwildfires = wildfires.exclude(containment_percent=None).order_by('-date_time_started', 'fire_name')[0:20]
     return render_to_response('403.html', {
         'calwildfires': calwildfires,
     })
 
 def custom_404(request):
-    calwildfires = CalWildfire.objects.all().order_by('-date_time_started', 'fire_name')
+    wildfires = CalWildfire.objects.all()
+    calwildfires = wildfires.exclude(containment_percent=None).order_by('-date_time_started', 'fire_name')[0:20]
     return render_to_response('404.html', {
         'calwildfires': calwildfires,
     })
 
 def custom_500(request):
-    calwildfires = CalWildfire.objects.all().order_by('-date_time_started', 'fire_name')
+    wildfires = CalWildfire.objects.all()
+    calwildfires = wildfires.exclude(containment_percent=None).order_by('-date_time_started', 'fire_name')[0:20]
     return render_to_response('500.html', {
         'calwildfires': calwildfires,
     })
