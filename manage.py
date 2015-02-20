8aa8c9da323c0989e3a4283db059bde346741247
Chris Keller <christopherlawrencekeller@gmail.com>
2015-02-20 10:20:34 -0800

    adds newrelic instrumentation for scraper and twitter management commands

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import newrelic.agent
 
 if __name__ == "__main__":
+    newrelic.agent.initialize("config/newrelic.ini")
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_production")
 
     from django.core.management import execute_from_command_line
