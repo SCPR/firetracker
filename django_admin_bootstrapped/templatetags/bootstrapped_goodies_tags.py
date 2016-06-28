7bde7f448698d8d300ce74642b9a15fa4f78061d
Chris Keller <christopherlawrencekeller@gmail.com>
2016-06-27 22:34:32 -0700

    updates to admin html templates to streamline some of the code

--- django_admin_bootstrapped/templatetags/bootstrapped_goodies_tags.py
+++ django_admin_bootstrapped/templatetags/bootstrapped_goodies_tags.py
@@ -1,5 +1,8 @@
 from django import template
 from django.template.loader import render_to_string, TemplateDoesNotExist
+import logging
+
+logger = logging.getLogger("firetracker")
 
 register = template.Library()
 
